1acc3ae1465fe707f21c8589172e3c8478829c90
Kevin Huayllas <KevinOmega01@gmail.com>
2023-07-10 17:29:54 -0400

    set up the enviroment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Canvas from "./Canvas";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./canvas.css";
+
+const Canvas = () => {
+
+    
+
+  return (
+    <div className="app-center">
+      <div className="title">
+        <h1 id="#title">Doping in Professional Bicycle Racing</h1>
+      </div>
+      <div id="canvas-container"></div>
+    </div>
+  );
+};
+
+export default Canvas;
