17787968be6403d347e62fc64d26f091e11ba499
Kevin Huayllas <KevinOmega01@gmail.com>
2023-07-10 18:04:34 -0400

    adding svg with d3

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,20 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import "./canvas.css";
+import * as d3 from "d3";
 
 const Canvas = () => {
   const [data, setData] = useState([]);
+  const [fetched, setFetched] = useState(false);
+  const [drawed, setDrawed] = useState(false);
 
   const fetchData = () => {
     fetch(
       "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
     )
       .then((response) => response.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .then(() => setFetched(true));
   };
 
-  useEffect(() => {
-    fetchData();
+  const drawData = useCallback(() => {
+    const svg = d3
+      .select("#canvas-container")
+      .append("svg")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .style("background", "red");
   }, []);
+
+  useEffect(() => {
+    if (!fetched) {
+      fetchData();
+    }
+  }, [fetched]);
+
+  useEffect(() => {
+    if (data.length && !drawed) {
+      setDrawed(true);
+      drawData();
+    }
+  }, [data, drawData, drawed]);
 
   return (
     <div className="app-center">
