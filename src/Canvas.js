2d6192e878bb1ca2a0e4563d78bd68e41234e69c
Kevin Huayllas <KevinOmega01@gmail.com>
2023-07-10 17:40:07 -0400

    adding fetching function

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./canvas.css";
 
 const Canvas = () => {
+  const [data, setData] = useState([]);
 
-    
+  const fetchData = () => {
+    fetch(
+      "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="app-center">
